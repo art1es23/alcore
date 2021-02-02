ce9d86e1d4c5a0c40d9a15ca2926c927df321f19
Denys Symak <dreamwinamounts@gmail.com>
2021-02-02 19:20:44 +0200

    main page 1.0

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import Slider from './res/_Slider';
+import Menu from './res/_Menu';
+// ===== init ======
+
+
+
+let slider = new Slider({
+    element: document.querySelector(".slider")
+});
+
+let menu = new Menu();
