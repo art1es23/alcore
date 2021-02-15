5909afb33a997269b18f230863056b4f0e9dae1e
Denys Symak <dreamwinamounts@gmail.com>
2021-02-15 17:37:50 +0200

    fixed-menu.2.0

--- src/js/res/_Menu.js
+++ src/js/res/_Menu.js
@@ -1,3 +1,5 @@
+import gsap from 'gsap';
+
 const toggleMenu = (container) => {
     class Menu {
         constructor() {
@@ -11,9 +13,66 @@ const toggleMenu = (container) => {
         }
 
         init() {
+
+            const logo = document.querySelector('.logo');
+            const logoLeft = logo.getBoundingClientRect().left;
+            const circleLogo = document.querySelector('circle.circle--logo');
+            const circleToggle = document.querySelector('circle.circle--toggle');
+
+            const hide = () => {
+                let tl = new gsap.timeline({
+                    paused: true
+                });
+                tl
+                    .to(circleLogo, 1.5, {
+                        scale: 1,
+                        y: 0,
+                        x: 0,
+                        yPercent: 0,
+                        xPercent: 0,
+                    })
+                    .to(circleToggle, 1.5, {
+                        scale: 1,
+                        y: 0,
+                        x: 0,
+                        yPercent: 0,
+                        xPercent: 0,
+                    }, '-=1.5');
+
+                return tl;
+            };
+
+            const show = () => {
+                let tl = new gsap.timeline({
+                    paused: true
+                });
+
+                tl
+                    .to(circleLogo, 1.5, {
+                        scale: 10,
+                        y: -500,
+                        x: -500,
+                        yPercent: 50,
+                        xPercent: 0,
+                    })
+                    .to(circleToggle, 1.5, {
+                        scale: 10,
+                        y: -500,
+                        x: -500,
+                        yPercent: -100,
+                        xPercent: 0,
+                    }, '-=1.5');
+                return tl;
+
+            };
+
             this.menuToogle.addEventListener('click', (e) => {
                 e.preventDefault();
-
+                if (!this.header.classList.contains('header--size')) {
+                    show().play();
+                } else {
+                    hide().play();
+                }
                 this.menuNav.classList.toggle('navigation--show');
                 this.menuToogle.classList.toggle('menu--toggle');
                 this.header.classList.toggle('header--size');
@@ -31,6 +90,7 @@ const toggleMenu = (container) => {
                 this.header.classList.remove('header--position', 'header--size');
 
                 if (scrollPage > 0) {
+                    show().play();
                     this.header.classList.add('header--position');
                     this.menuInner.classList.remove('navigation--align');
                     this.menuNav.classList.remove('navigation--show');
@@ -38,17 +98,21 @@ const toggleMenu = (container) => {
 
                     if (this.menuNav.classList.contains('navigation--show')) {
                         this.menuInner.classList.add('navigation--align');
+                        hide().play();
+
                     }
                 } else {
+                    hide().play();
+
                     this.menuNav.classList.remove('navigation--show');
                     this.menuToogle.classList.remove('menu--toggle');
                     this.menuInner.classList.remove('navigation--align');
                 }
 
             });
-
-            console.log('SOME FUNCTION INIT');
         }
+
+
     }
 
     let togglemenu = new Menu();
